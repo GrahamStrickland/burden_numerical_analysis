65b312b97c4515ea0de6492bddae2cceddb69d45
Graham Strickland <grahamhstrickland@gmail.com>
2023-01-09 20:50:09 +0200

    Add test/test_secant.py and fixed_position

---
+++ ch02/false_position/cli.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+import argparse
+import math
+from typing import TextIO
+
+from false_position import false_position
+
+
+def predefined_function(x: float) -> float:
+    return math.cos(x) - x
+
+
+def parse_file_input(input_file: TextIO) -> dict:
+    """Parse the initial approximations, tolerance, and maximum number of inputs supplied in a CSV file."""
+    vals = input_file.read().split(',')
+    p_0: float = float(vals[0])
+    p_1: float = float(vals[1])
+    tol: float = float(vals[2])
+    n_0: int = int(vals[3])
+
+    return {'p_0': p_0, 'p_1': p_1, 'tol': tol, 'n_0': n_0}
+
+
+def check_input_params(p_0: float, p_1: float, tol: float, n_0: int) -> None:
+    """Check that the initial approximations are not equal and that the tolerance and the number of
+    iterations are positive."""
+    if p_0 == p_1:
+        raise IOError("p_0 cannot be equal to p_1.")
+
+    # check that TOL > 0
+    if tol <= 0.0:
+        raise IOError("Tolerance must be a positive number.")
+
+    # check that n_0 > 0
+    if n_0 <= 0:
+        raise IOError("Maximum number of iterations must be a positive integer.")
+
+
+def main():
+    parser = argparse.ArgumentParser(description=false_position.__doc__)
+
+    parser.add_argument(
+        "p_0",
+        nargs='?',
+        default=None,
+        type=float,
+        help="The first initial approximation."
+    )
+    parser.add_argument(
+        "p_1",
+        nargs='?',
+        default=None,
+        type=float,
+        help="The second initial approximation."
+    )
+    parser.add_argument(
+        "--function",
+        nargs='?',
+        default=None,
+        type=str,
+        help="A function continuous in some neighbourhood of p_0."
+    )
+    parser.add_argument(
+        "--tol",
+        nargs='?',
+        default=1e-6,
+        type=float,
+        help="The tolerance for the function."
+    )
+    parser.add_argument(
+        "--n_0",
+        nargs='?',
+        default=100,
+        type=int,
+        help="The maximum number of iterations."
+    )
+    parser.add_argument(
+        "--input_file",
+        nargs='?',
+        default=None,
+        type=argparse.FileType('r'),
+        help="The name of the input file."
+    )
+    parser.add_argument(
+        "--output_file",
+        nargs='?',
+        default=None,
+        type=argparse.FileType('w'),
+        help="The name of the output file."
+    )
+    parser.add_argument(
+        "--table_output",
+        action="store_true",
+        help="Flag for table output."
+    )
+
+    args = parser.parse_args()
+
+    if args.function:
+        function = lambda x: eval(args.function)
+    else:
+        function = predefined_function
+
+    if args.input_file:
+        if args.p_0:
+            raise IOError("If an input file has been defined, no other input parameters must be defined.")
+        params = parse_file_input(args.input_file)
+        p_0, p_1, tol, n_0 = params["p_0"], params["p_1"], params["tol"], params["n_0"]
+    else:
+        if args.p_0 is None or args.p_1 is None:
+            raise IOError(
+                "If an input file has not been defined, the initial approximation must be specified as an argument."
+            )
+        p_0, p_1, tol, n_0 = args.p_0, args.p_1, args.tol, args.n_0
+
+    check_input_params(p_0, p_1, tol, n_0)
+
+    if args.output_file:
+        print("")
+        args.output_file.write("This is the method of False Position.\n")
+        _ = false_position(
+            function=function, p_0=p_0, p_1=p_1, tol=tol, n_0=n_0,
+            file=args.output_file, table_output=True
+        )
+        args.output_file.close()
+    else:
+        print("This is the method of False Position.")
+        _ = false_position(
+            function=function, p_0=p_0, p_1=p_1, tol=tol, n_0=n_0,
+            file=None, table_output=args.table_output
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ ch02/test/test_secant.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+import math
+from pytest import approx
+
+from ..secant.secant import secant_method
+
+
+def test_secant_method_trig() -> None:
+    def f(x: float) -> float:
+        return math.cos(x) - x
+
+    p_0: float = 0.5
+    p_1: float = 0.7853981634
+    tol: float = 1e-10
+    n_0: int = 5
+
+    obs = secant_method(f, p_0, p_1, tol, n_0)
+    exp: float = 0.7390851332
+
+    assert obs == approx(exp, abs=tol)
+
+
+def test_secant_method_poly() -> None:
+    def f(x: float) -> float:
+        return x**2 - 6.0
+
+    p_0: float = 3.0
+    p_1: float = 2.0
+    tol: float = 1e-5
+    n_0: int = 3
+
+    obs = secant_method(f, p_0, p_1, tol, n_0)
+    exp: float = 2.45454
+
+    assert obs == approx(exp, abs=tol)
+
+
+def test_secant_method_poly_2() -> None:
+    def f(x: float) -> float:
+        return x**3 - 2.0*x**2 - 5.0
+
+    p_0: float = 1.0
+    p_1: float = 4.0
+    tol: float = 1e-5
+    n_0: int = 11
+
+    obs = secant_method(f, p_0, p_1, tol, n_0)
+    exp: float = 2.69065
+
+    assert obs == approx(exp, abs=tol)
+
+
+def test_secant_method_poly_3() -> None:
+    def f(x: float) -> float:
+        return x**3 + 3.0*x**2 - 1.0
+
+    p_0: float = -3.0
+    p_1: float = -2.0
+    tol: float = 1e-5
+    n_0: int = 7
+
+    obs = secant_method(f, p_0, p_1, tol, n_0)
+    exp: float = -2.87939
+
+    assert obs == approx(exp, abs=tol)
+
+
+def test_secant_method_trig_2() -> None:
+    def f(x: float) -> float:
+        return x - math.cos(x)
+
+    p_0: float = 0.0
+    p_1: float = 1.57080
+    tol: float = 1e-5
+    n_0: int = 6
+
+    obs = secant_method(f, p_0, p_1, tol, n_0)
+    exp: float = 0.73909
+
+    assert obs == approx(exp, abs=tol)
+
+
+def test_secant_method_trig_3() -> None:
+    def f(x: float) -> float:
+        return x - 0.8 - 0.2*math.sin(x)
+
+    p_0: float = 0.0
+    p_1: float = 1.57080
+    tol: float = 1e-5
+    n_0: int = 5
+
+    obs = secant_method(f, p_0, p_1, tol, n_0)
+    exp: float = 0.96433
+
+    assert obs == approx(exp, abs=tol)
