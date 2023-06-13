1adfd29ec296b3a0b23a3cbbce9ff55a542233ea
Graham Strickland <grahamhstrickland@gmail.com>
2023-06-13 08:27:43 +0200

    Add Pade Rational Approximation to ch08

---
+++ ch08/pade_rational_approximation/cli.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+import argparse
+import math
+from typing import TextIO
+
+from pade_rational_approximation import pade_rational_approximation
+
+
+def predefined_function(x: float) -> float:
+    """Predefined function in case user does not define a function."""
+    return math.exp(-x)
+
+
+def parse_file_input(input_file: TextIO) -> dict:
+    """Parse the values for m, n, and Maclaurin polynomial coefficients.""" 
+    vals = input_file.read().split(',')
+    m = int(vals[0])
+    n = int(vals[1])
+    coeffs = []
+    for i in range(2, len(vals)):
+        coeffs.append(vals[i])
+
+    return {'m': m, 'n': n, 'maclaurin_coeffs': coeffs}
+
+
+def check_input_params(
+        m: int, n: int, coeffs: list(float)
+                       ) -> list[int, int, list(float)]:
+    """Check that m and n are nonnegative and that the Maclaurin coefficients
+    are of order N = m + n."""
+    if m < 0 or n < 0:
+        raise IOError("m and n must be nonnegative integers.")
+
+    if len(coeffs) != m + n:
+        raise IOError(f"Please specify {m + n} Maclaurin coefficients.")
+
+    return [m, n, coeffs]
+
+
+def main():
+    parser = argparse.ArgumentParser(description=pade_rational_approximation.__doc__)
+
+    parser.add_argument(
+        "m",
+        nargs='?',
+        default=None,
+        type=int,
+        help="The degree of the denominator polynomial."
+    )
+    parser.add_argument(
+        "n",
+        nargs='?',
+        default=None,
+        type=int,
+        help="The degree of the numerator polynomial."
+    )
+    parser.add_argument(
+        "--function",
+        nargs='?',
+        default=None,
+        type=str,
+        help="A function f(t,y) continuous on the interval [a, b]."
+    )
+    parser.add_argument(
+        "--input-file",
+        nargs='?',
+        default=None,
+        type=argparse.FileType('r'),
+        help="The name of the input file."
+    )
+    parser.add_argument(
+        "--output-file",
+        nargs='?',
+        default=None,
+        type=argparse.FileType('w'),
+        help="The name of the output file."
+    )
+
+    args = parser.parse_args()
+
+    if args.function:
+        function = lambda x: eval(args.function)
+    else:
+        function = predefined_function
+
+    if args.input_file:
+        if args.m or args.n:
+            raise IOError(
+"""If an input file has been defined, no other input parameters must be defined."""
+                )
+        params = parse_file_input(args.input_file)
+        m, n, coeffs = params["m"], params["n"], params["maclaurin_coeffs"]
+    else:
+        if args.m is None or args.n is None:
+            raise IOError(
+"""If an input file has not been defined, m and n must be specified as arguments."""
+                )
+        m, n = args.m, args.n
+        coeffs = None
+
+    check_input_params(m, n, coeffs)
+
+    if args.output_file:
+        args.output_file.write(
+            "This is the Pade Rational Approximation.\n"
+                )
+        _ = pade_rational_approximation(
+            function=function, m=m, n=n, file=args.output_file, 
+            maclaurin_coeffs=coeffs 
+        )
+        args.output_file.close()
+    else:
+        print(
+            "This is the Pade Rational Approximation."
+                )
+        _ = pade_rational_approximation(
+            function=function, m=m, n=n, file=None, maclaurin_coeffs=coeffs 
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ ch08/pade_rational_approximation/pade_rational_approximation.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import math
+
+from collections.abc import Callable
+from typing import TextIO
+
+
+LONG_BORDER = 132
+SHORT_BORDER = 84
+
+
+def pade_rational_approximation(
+        function: Callable(float), m: int, n: int, 
+        maclaurin_coeffs: list(float) = None, file: TextIO = None
+) -> list[list[float]]:
+    """To obtain the rational approximation r(x) = p(x)/q(x) 
+    = sum_i=0^n p_ix^i / sum_j=0^m q_j x^j for a given function f(x):
+    INPUT nonnegative integers m and n.
+    OUTPUT coefficients q_0, q_1, ..., q_m and p_0, p_1, ..., p_n."""
+
+    # STEP 1: Set N = m + n.
+
+    # STEP 2: For i = 0, 1, ..., N set a_i = f^(i)(0)/i!.
+    #         (The coefficients of the Maclaurin polynomial are a_0, ..., a_N,
+    #         which could be input instead of calculated.)
+
+    # STEP 3: Set q_0 = 1;
+    #             p_0 = a_0.
+
+    # STEP 4: For i = 1, 2, ..., N do Steps 5-10. (Set up a linear system with 
+    #         matrix B.)
+
+        # STEP 5: For j = 1, 2, ..., i - 1
+        #           if j <= n then set b_i,j = 0.
+
+        # STEP 6: If i <= n then set b_i,i = 1.
+
+        # STEP 7: For j = i+1, i+2, ..., N set b_i,j = 0.
+
+        # STEP 8: For j = 1, 2, ..., i
+        #           if j <= m then set b_i,n+j = -a_i-j.
+
+        # STEP 9: For j = n+i+1, n+i+2, ..., N set b_i,j = 0.
+
+        # STEP 10: Set b_i,N+1 = a_i.
+
+    # (Steps 11-22 solve the linear system using partial pivoting.)
+    # STEP 11: For i = n+1, n+2, ..., N-1 do Steps 12-18.
+
+        # STEP 12: Let k be the smallest integer with i <= k <= N and |b_k,i|
+        #           = max_i<=j<=N |b_j,i|.
+        #           (Find pivot element.)
+
+        # STEP 13: If b_k,i = 0 then OUTPUT("The system is singular");
+        #                            STOP.
+
+        # STEP 14: If k!= i then (Interchange row i and row k.)
+        #           for j = i, i+1, ..., N+1 set
+        #           b_COPY = b_i,j;
+        #           b_i,j = b_k,j;
+        #           b_k,j = b_COPY.
+
+        # STEP 15: For j = i+1, i+2, ..., N do Steps 16-18. (Perform elimination.)
+
+            # STEP 16: Set xm = b_j,i / b_i,i.
+
+            # STEP 17: For k = i+1, i+2, ..., N+1
+            #           set b_j,k = b_j,k - xm*b_i,k.
+
+            # STEP 18: Set b_j,i = 0.
+
+    # STEP 19: If b_N,N = 0 then OUTPUT("The system is singular");
+    #                            STOP.
+
+    # STEP 20: If m > 0 then set q_m = b_N,N+1 / b_N,N. 
+    #           (Start backward substitution.)
+
+    # STEP 21: For i = N-1, N-2, ..., n+1 
+    #           set q_i-n = b_i,N+1 - sum_j=i+1^N b_i,j * q_j-n / b_i,i.
+
+    # STEP 22: For i = n, n-1, ..., 1 set p_i = b_i,N+1 - sum_j=n+1^N b_i * q_j-n.
+
+    # STEP 23: OUTPUT(q_0, q_1, ..., q_m, p_0, p_1, ..., p_n);
+    #          STOP. (The procedure was successful.)
